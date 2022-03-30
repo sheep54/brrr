b303d65011ec9c6e76a0b917f7813542136f9680
sheep54 <86818520+sheep54@users.noreply.github.com>
2022-03-30 00:39:41 -0700

    initial

---
+++ js/scripts.js
@@ -0,0 +1,586 @@
+let web3;
+let abiToken = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "Weth",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "factory",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "x",
+				"type": "uint256"
+			}
+		],
+		"name": "PRBMathUD60x18__LogInputTooSmall",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "prod1",
+				"type": "uint256"
+			}
+		],
+		"name": "PRBMath__MulDivFixedPointOverflow",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "prod1",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "denominator",
+				"type": "uint256"
+			}
+		],
+		"name": "PRBMath__MulDivOverflow",
+		"type": "error"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "TransferReceived",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_destAddr",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "TransferSent",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "CreditbalanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "DebitbalanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "EntryFee",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "RankID",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "Register",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "RegistryCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "balance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "pair",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "rewardLPs",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "updateFee",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address payable",
+				"name": "destAddr",
+				"type": "address"
+			}
+		],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	}
+]
+
+let tokenContract;
+let userAddress;
+
+const ethereumButton = document.getElementById('connect');
+const registerButton = document.getElementById('register');
+const rewardlpsButton = document.getElementById('rewardlps');
+const StakeTokenButton = document.getElementById('tokenstakebutton');
+
+ethereumButton.addEventListener('click', async () => {
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  ethereumButton.innerHTML = 'Wallet Connected';
+  userAddress = accounts[0];
+  console.log("User "+accounts[0]+" has connected.");
+  web3 = new Web3(ethereum);
+  init();
+
+  let chainId = await ethereum.request({ method: 'eth_chainId'});
+  if (chainId != "0x1") {
+    console.log("Current chainId is "+chainId)
+    alert("Please switch to ethereum mainnet.");
+  }
+});
+
+ethereum.on('chainChanged', (chainId) => {
+    console.log("chain changed to "+chainId);
+    if (chainId != "0x1") {
+        alert("Please switch to ethereum mainnet.");
+    }
+    init();
+});
+
+ethereum.on('accountsChanged', function (accounts) {
+    userAddress = accounts[0];
+    console.log("User "+accounts[0]+" has connected.");
+    init();
+
+});
+
+registerButton.addEventListener('click', async() => {
+    registerAmount = await tokenContract.methods.EntryFee().call();
+    await web3.eth.sendTransaction({from: userAddress, to: tokenContract._address, value: registerAmount, gas: 135000});
+    await fetchValues();
+});
+
+
+rewardlpsButton.addEventListener('click', async() => {
+    await tokenContract.methods.rewardLPs().send({from: userAddress});
+    await fetchValues();
+
+});
+
+async function init() {
+    tokenContract = await new web3.eth.Contract(abiToken, "address");
+
+    fetchValues()
+    setInterval(function(){ fetchValues(); }, 5000);
+}
+
+async function fetchValues() {
+    const rank = await tokenContract.methods.RankID(userAddress).call();
+    const balance = BigNumber(await tokenContract.methods.balanceOf(userAddress).call()).dividedBy(BigNumber(1e+18)).toFixed(3).toString(10);
+    const totalUsers = await tokenContract.methods.RegistryCount().call();
+    const totalSupply = BigNumber(await tokenContract.methods.totalSupply().call()).dividedBy(BigNumber(1e+18)).toFixed(2).toString(10);
+    document.getElementById("SystemInfo").innerHTML = ("Total Users: "+totalUsers+" | Total Supply: "+totalSupply);
+    const fee = BigNumber(await tokenContract.methods.EntryFee().call()).dividedBy(BigNumber(1e+18)).toFixed(2).toString(10);
+    const signupAward = GetAward(parseInt(totalUsers)+1,parseInt(totalUsers)+1)
+    document.getElementById("RegistrationInfo").innerHTML = ("Registration Fee: "+fee+" Ξ | Signup Award: "+signupAward+" BRRR"); 
+
+    let NextAward = Math.round((GetAward(parseInt(rank),parseInt(totalUsers)+1) - GetAward(parseInt(rank),parseInt(totalUsers)))*100)/100;
+    if (rank == 0) {
+        NextAward = 0;
+        document.getElementById("UserInfo").innerHTML = ("Ranking: Unregistered | Balance: "+balance+" BRRR");
+    }
+    else {
+        document.getElementById("UserInfo").innerHTML = ("Ranking: #"+rank+" | Balance: "+balance+" BRRR");
+    }
+    document.getElementById("NextAward").innerHTML = ("Next BRRR Reward: "+NextAward);
+}
+
+function GetAward(RankID, TotalUsers)
+{
+   TotalArea = Math.log(TotalUsers) + 1;
+   RankArea = 1;
+    if (RankID > 1) { 
+	RankArea = Math.log(RankID) - Math.log(RankID - 1)    
+    }
+    RankPct = RankArea / TotalArea;
+    CoinBase = TotalUsers*TotalUsers;
+    return Math.round(CoinBase * RankPct*100)/100;    
+} 
+
